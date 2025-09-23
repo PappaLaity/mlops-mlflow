5cfefa25ff0916fbe35125fd729ac7511ae2566e
Pappalaity <pappalaity@gmail.com>
2025-09-23 16:36:01 +0000

    main file adding environment variable

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from dotenv import load_dotenv
+import os
 import mlflow
 from sklearn.datasets import load_diabetes
 from sklearn.ensemble import RandomForestRegressor
@@ -6,8 +8,14 @@ from sklearn.model_selection import train_test_split
 from functions.iris_functions import evaluate_iris_lr_model, load_iris_data, train_iris_lr_model
 
 if __name__ == "__main__":
+
+    load_dotenv()  # Loads variables from .env file
+
+    # my_variable = os.getenv('MY_VARIABLE')
+    # database_url = os.getenv('DATABASE_URL')
+
     mlflow.autolog()
-    mlflow.set_tracking_uri(uri="http://localhost:2500")
+    # mlflow.set_tracking_uri("http://localhost:5000")
     with mlflow.start_run():
         # X_train, X_test, y_train, y_test = load_iris_data()
 
@@ -24,3 +32,4 @@ if __name__ == "__main__":
         rf = RandomForestRegressor(n_estimators=100, max_depth=6, max_features=3)
         # MLflow triggers logging automatically upon model fitting
         rf.fit(X_train, y_train)
+
