ef8686a65e321523e9278f09f302ff5e4e5d254a
Pappalaity <pappalaity@gmail.com>
2025-09-23 16:39:43 +0000

    update evaluate model functions

---
+++ main manual log.py
@@ -0,0 +1,67 @@
+import mlflow
+from functions.iris_functions import (
+    evaluate_iris_lr_model,
+    load_iris_data,
+    train_iris_dt_model,
+    train_iris_lr_model,
+)
+from mlflow.models import infer_signature
+
+
+if __name__ == "__main__":
+    lr_params = {
+        "solver": "lbfgs",
+        # "max_iter": 1000,
+        "max_iter": 30,
+        "random_state": 8888,
+        # "random_state": 42,
+    }
+    dt_params = {
+        "criterion":"entropy",
+        "random_state": 8888,
+        # "random_state": 42,
+    }
+    
+    X_train, X_test, y_train, y_test = load_iris_data()
+
+    models = [train_iris_lr_model, train_iris_dt_model]
+    models_params = [lr_params,dt_params]
+    models_names = ["iri_logistic_regression_model","iris_decision_trees_model"]
+    mlflow.set_tracking_uri(uri="http://127.0.0.1:5000")
+
+    for idx, model in enumerate(models):
+
+        res_model = model(X_train, y_train, models_params[idx])
+
+        y_pred = res_model.predict(X_test)
+
+        _,_,accuracy = evaluate_iris_lr_model(y_test, y_pred)
+
+        print(f"Model Accuracy: {accuracy:.4f}")
+
+        mlflow.set_experiment("Experiment Models Manual-log")
+
+        with mlflow.start_run():
+            # Log the hyperparameters
+            mlflow.log_params(models_params[idx])
+
+            # Log the loss metric
+            mlflow.log_metric("accuracy", accuracy)
+
+            # Set a tag that we can use to remind ourselves what this run was for
+            mlflow.set_tag("Training Info", models_names[idx])
+
+            # Infer the model signature
+            signature = infer_signature(X_train, res_model.predict(X_train))
+
+            # Log the model
+            model_info = mlflow.sklearn.log_model(
+                sk_model=res_model,
+                name=models_names[idx],
+                signature=signature,
+                input_example=X_train,
+                registered_model_name="tracking-model-"+models_names[idx],
+            )    
+
+
+    
